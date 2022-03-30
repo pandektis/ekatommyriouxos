6fdf8ab5487b3f92be0fd6e761bb0700bd6d65ac
pandektis <37712125+pandektis@users.noreply.github.com>
2022-03-30 09:02:32 +0300

    Fix typo in Player, add field for questions, add method calc_mo

--- Player.py
+++ Player.py
@@ -1,10 +1,17 @@
 class Player:
-    def __init(self, name):
+    def __init__(self, name):
         self.name = name
         self.poso = 0
+        self.num_questions = 0
         self.total_time = 0
         self.m_o = 0
         self.daep = 0
 
     def __str__(self):
         return f"{self.name}, {self.poso}€, {self.total_time}sec, {self.m_o}sec/q, {self.daep}"
+
+    def calc_mo(self):
+        try:
+            self.m_o = self.total_time / self.num_questions
+        except ZeroDivisionError as Z:
+            self.m_o = 0
