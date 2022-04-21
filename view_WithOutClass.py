2f0cc9491f01ce947dd88d80c70d4e8867964fa6
xsystemgr <xsystemgr@gmail.com>
2022-04-22 00:19:34 +0300

    view without Class //changelog 1.Rename file name 2.Ask for user name 3.Answere keyboard key detection 4.remove some def

--- view_WithOutClass.py
+++ view_WithOutClass.py
@@ -1,5 +1,7 @@
 import os
 import pygame
+import tkinter as tk
+from tkinter import simpledialog
 import time
 import sys
 import math
@@ -18,15 +20,14 @@ screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 timeP = pygame.time
 
-bank = int(1000)
+
 qdata = str("--------------------------")
 font = pygame.font.Font(None, 80)
 text = 'Fonty'
 size = font.size(text)
 
-fg = 250, 250, 250  # font color
+fg = 250, 250, 250
 bg = 5, 5, 5
-
 
 question_sys_font = pygame.font.SysFont("Tahoma", 50)
 answers_sys_font = pygame.font.SysFont ("Tahoma",30)
@@ -34,7 +35,7 @@ bank_sys_font = pygame.font.SysFont ("Arial",30)
 
 
 background_image = pygame.image.load("img/lobby-2.png").convert()
-background_image_blank = pygame.image.load("img/lobby-2.png").convert()
+background_image_blank = pygame.image.load("img/millionaire_logo_full.png").convert()
 icon = pygame.image.load('img/millionaire_logo.png')
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Millionaire")
@@ -44,47 +45,16 @@ pygame.mixer.music.play(0, 0.0)
 pygame.mixer.music.set_volume(0.2)
 
 
-
-
-
-
-
 def timerFunc(index):
     index_old = index
     index = index + 1
     return index
-
-
-def load_images(path):
-    """
-        Φορτώνει όλες τις εικόνες στον κατάλογο.
-    """
-    images = []
-    images_names = []
-
-    for file_name in os.listdir(path):
-        image_name = file_name
-        images_names.append(image_name)
-    images_names = sorted(images_names)
-    print(images_names)
-
-    for file_name in images_names:
-        image = pygame.image.load(path + os.sep + file_name).convert()
-        images.append(image)
-    return images
-
-
-
-
 
 def draw_player_data(name):
    textSurf = bank_sys_font.render("Ονομα :" + (name), 1, fg)
    textRect = textSurf.get_rect()
    textRect.center = (150, HEIGHT-30)
    screen.blit(textSurf, textRect)
-
-
-
 
 def draw_question(qdata):
     textSurf = question_sys_font.render((qdata), 1, fg)
@@ -116,32 +86,37 @@ def draw_ansD(ansDdata):
     textRect.center = ((WIDTH / 1.53), (HEIGHT / (1.25)))
     screen.blit(textSurf, textRect)
 
+def askName():
+    global inputname
+    inputname = simpledialog.askstring(title="Millionaire",
+                                      prompt="Καλώς ήρθες , εισήγαγε το όνομα σου για να συνεχίσεις :")
 
 
-
-
-
-
+#------------------------------------------------------MainProgram-------------------------------------------------
 def main():
     balance = [0, 100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
     running = True
+
     pygame.display.update()
     global background_image_blank
     global background_image
-    name = "KOLOKOTRONIS"
+
     qdata = "Q11111111111"
     ansAdata = "A111111111"
     ansBdata = "B222222222"
     ansCdata = "C333333333"
     ansDdata = "D444444444"
+    screen.blit(background_image_blank, [0, 0])
+    pygame.display.update()
+    askName()
 
+    name = inputname
 
 
 
     while (running):
+        screen.blit(background_image, [0, 0])
 
-
-        screen.blit(background_image_blank, [0, 0])
 
         draw_question(qdata)
         draw_ansA(ansAdata)
@@ -151,18 +126,23 @@ def main():
 
         draw_player_data(name)
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit();  # sys.exit() if sys is imported
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    print("PRESS A")
+                if event.key == pygame.K_b:
+                    print("PRESS B")
+                if event.key == pygame.K_c:
+                    print("PRESS C")
+                if event.key == pygame.K_d:
+                    print("PRESS D")
+
+
+
         pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-
-                running = False
-                pygame.quit()
 
 
 
