9900d692a1b0404be72d711c78eb074b5d7c1988
pandektis <37712125+pandektis@users.noreply.github.com>
2022-04-27 17:22:54 +0300

    Organizational change, new folder
    Move some older files to new folder
    Add pictures

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from View import View
 from Model import Model
 
 
-def start():
-    v = View()
-    m = Model()
-    c = Controller(v, m)
-    # Προς αφαίρεση 2 γραμμές, δοκιμή σύνδεσης View με Controller
-    print(c)
-    c.hello("main")
+
+v = View()
+m = Model()
+c = Controller(v, m)
+# Προς αφαίρεση 2 γραμμές, δοκιμή σύνδεσης View με Controller
+print(c)
+c.hello("main")
 
 
-if __name__ == '__main__':
-    start()
+
+
